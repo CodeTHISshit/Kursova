1de692d25a3598308499eaff4e692171b10a2ece
CodeThisShit <cool.kasad@gmail.com>
2019-11-16 17:48:42 +0200

    version 0.1.2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,54 +9,99 @@
     <meta name="generator" content="Jekyll v3.8.5">
     <title>Starter Template · Bootstrap</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/starter-template/">
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="../../public/bootstrap/css/bootstrap.min.css">
-
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}">
 
 
     <!-- Custom styles for this template -->
     <link href="../../public/css/style_index.css" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="#">TrainWorld</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="mb-1 navbar navbar-expand-lg  fixed-top text-white">
+    <a id="Logo"  class=" text-white navbar-brand" href="#">PoizdaTYT</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+            aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
+    <div class=" collapse navbar-collapse" id="navbarSupportedContent-333">
+        <ul class="navbar-nav ml-auto ">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Головна <span class="sr-only">(current)</span></a>
+                <a class="nav-link text-white" href="#">Поїзда
+                    <span class="sr-only">(current)</span>
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Автобуси</a>
+                <a class="nav-link text-white" href="#">Автобуси</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a class="nav-link text-white" href="ContactUs.html">Літаки</a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">Dropdown
+                </a>
+                <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <a class="dropdown-item" href="#">Something else here</a>
                 </div>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        <ul class="navbar-nav ml-auto nav-flex-icons">
+            <li class="nav-item">
+                <a class="nav-link waves-effect waves-light">
+                    <i class="fab fa-twitter"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link waves-effect waves-light">
+                    <i class="fab fa-google-plus-g"></i>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-3" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-default"
+                     aria-labelledby="navbarDropdownMenuLink-333">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
     </div>
 </nav>
-<div class="row" >
-    <div class="col-12">
+<section class="not-adaptive">
+    <div id="SearchRow" class="row justify-content-center mt-5" >
+        <form id="SearchField" class="text-center  p-5 mt-5" action="#!">
 
+            <p class="h4 mb-4 text-white">Замовити квиток</p>
+            <div class="form-row mb-4">
+                <div class="col  mr-5">
+                    <!-- First name -->
+                    <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="Відправна точка">
+                </div>
+                <div class="col mr-3 ml-5">
+                    <!-- Last name -->
+                    <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Кінцева точка">
+                </div>
+                <div class="col">
+                    <input type="email" id="defaultRegisterFormEmail" class="form-control mb-4" placeholder="Дата">
+                </div>
+            </div>
+
+            <div class="row justify-content-end" >
+                <a id="btn" href="TrainChoose.html">Замовити</a>
+            </div>
+
+        </form>
     </div>
-</div>
+</section>
+
 
 </body>
 </html>
